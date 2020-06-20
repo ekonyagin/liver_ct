1f6fae8e813e01b09dbc2fdcdfc9fd8f826fc639
ekonyagin <ekserviceek@gmail.com>
2020-06-20 14:30:09 +0300

    changed function output to (min_z, max_z)

--- baseline_experiments/liver_localization.py
+++ baseline_experiments/liver_localization.py
@@ -19,19 +19,8 @@ def SelectSlice(pred,
     min_z = np.maximum(min_z-epsilon,0)
     max_z = np.minimum(max_z+epsilon, len(sum_distr))
 
-    sum_x = pred.sum(axis=2).sum(axis=0)
-    min_x = np.argwhere(sum_x>100).min()
-    max_x = np.argwhere(sum_x>100).max()       
-    min_x = np.maximum(min_x-epsilon,0)
-    max_x = np.minimum(max_x+epsilon, len(sum_x))
-
-    sum_y = pred.sum(axis=2).sum(axis=1)
-    min_y = np.argwhere(sum_y>100).min()
-    max_y = np.argwhere(sum_y>100).max()
-    min_y = np.maximum(min_y-epsilon,0)
-    max_y = np.minimum(max_y+epsilon, len(sum_y))
-
-    pred = pred[min_y:max_y, min_x: max_x, min_z:max_z]
+    #pred = pred[: , min_x: max_x, min_z:max_z]
     #print(min_y, max_y, sum_y.shape)
     #pred.shape
-    return pred
+    #return pred
+    return min_z, max_z
